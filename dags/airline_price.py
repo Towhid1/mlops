64776e67b3cdae16ce8cbfd12d8fdca9e03ee856
Towhid1 <nurulaktertowhid@gmail.com>
2023-08-26 22:39:45 +0600

    week 6

--- dags/airline_price.py
+++ dags/airline_price.py
@@ -1,13 +1,37 @@
+# airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
+# import for training
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error
+
+# data preparation
 import pandas as pd
 from scipy.stats import yeojohnson
 import sklearn.preprocessing as preproc
 import os
 
+
 def data_pre(ti, path="/opt/airflow/data/raw_data/Clean_Dataset.csv"):
+    """
+    Prepare and preprocess the dataset for training.
+
+    This function reads a CSV file located at the given path, performs various data
+    preprocessing steps, including label-encoding, one-hot encoding, transformation for normal distribution,
+    standardization, and outlier removal. It saves the preprocessed data for economy and business
+    class separately and provides context information for downstream tasks.
+
+    Args:
+        ti (TaskInstance): The Airflow TaskInstance object.
+        path (str, optional): Path to the CSV file containing the raw dataset. Default is
+            "/opt/airflow/data/raw_data/Clean_Dataset.csv".
+
+    Returns:
+        None
+    """
     data = pd.read_csv(path, index_col=0)
     # Encode the ordinal variables "stops" and "class".
     data["stops"] = (
@@ -80,10 +104,66 @@ def data_pre(ti, path="/opt/airflow/data/raw_data/Clean_Dataset.csv"):
     data.iloc[cls_bsn].to_csv("/opt/airflow/data/feature/bus_features.csv", index=False)
 
     context_dict = {
-        "business": "/opt/airflow/data/feature/bus_features.csv",
+        "business": "/opt/airflow/data/feature/bsn_features.csv",
         "economy": "/opt/airflow/data/feature/eco_features.csv",
     }
     ti.xcom_push(key="data_preparation_context", value=context_dict)
+    return None
+
+
+def bsn_training(ti):
+    """
+    Perform training and evaluation of a K-Nearest Neighbors Regressor model
+    for predicting prices using business-class features.
+
+    Args:
+        ti (TaskInstance): The Airflow TaskInstance object.
+
+    Returns:
+        None
+    """
+    dir_dic = ti.xcom_pull(key='data_preparation_context')
+    filename = dir_dic['business']
+    feature = pd.read_csv(filename)
+    target = feature.pop("price")
+    x_train, x_test, y_train, y_test = train_test_split(
+        feature, target, random_state=1, test_size=0.3,
+        shuffle=True)
+
+    model = KNeighborsRegressor()
+    trained_model = model.fit(x_train, y_train)
+    y_pred = trained_model.predict(x_test)
+    mae = mean_absolute_error(y_test, y_pred)
+
+    print(f"business class mean_absolute_error: {mae}")
+    return None
+
+
+def eco_training(ti):
+    """
+    Perform training and evaluation of a K-Nearest Neighbors Regressor model
+    for predicting prices using economy-class features.
+
+    Args:
+        ti (TaskInstance): The Airflow TaskInstance object.
+
+    Returns:
+        None
+    """
+    dir_dic = ti.xcom_pull(key='data_preparation_context')
+    filename = dir_dic['economy']
+    feature = pd.read_csv(filename)
+    target = feature.pop("price")
+    x_train, x_test, y_train, y_test = train_test_split(
+        feature, target, random_state=1, test_size=0.3,
+        shuffle=True)
+
+    model = KNeighborsRegressor()
+    trained_model = model.fit(x_train, y_train)
+    y_pred = trained_model.predict(x_test)
+    mae = mean_absolute_error(y_test, y_pred)
+
+    print(f"economy class mean_absolute_error: {mae}")
     return None
 
 
@@ -97,3 +177,13 @@ with airline_dag:
     data_preparation_task = PythonOperator(
         task_id="data_preparation", python_callable=data_pre, provide_context=True,
     )
+
+    eco_training_task = PythonOperator(
+        task_id="eco_training_task", python_callable=eco_training, provide_context=True
+    )
+
+    bsn_training_task = PythonOperator(
+        task_id="bsn_training_task", python_callable=bsn_training, provide_context=True
+    )
+
+    data_preparation_task >> [bsn_training, eco_training]
